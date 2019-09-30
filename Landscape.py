a32c2d66b3b8ad8624cffa1ab3c4a1e975fdc3f9
100111079 <kylervillarreal@icloud.com>
2019-09-30 09:02:32 -0500

    Playing with the maths and the Octothorpes

--- Landscape.py
+++ Landscape.py
@@ -1,3 +1,11 @@
 print("hello world")
 print("howdy, y'alL")
 print("I like typing this")
+
+# math skills demo
+
+print(" i will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now I will count the eggs, 7")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 +6)
